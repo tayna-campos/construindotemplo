df4558c6d368a9e0c6260afc99239397b658e6ed
tayna-campos <83303513+tayna-campos@users.noreply.github.com>
2026-05-26 14:31:08 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,34 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getFirestore, doc, updateDoc, increment, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-const firebaseConfig={apiKey:"AIzaSyDEN1XSKmtIi-DlLihv65GOdNa6v0oMcfw",authDomain:"construindo-o-templo.firebaseapp.com",projectId:"construindo-o-templo",storageBucket:"construindo-o-templo.firebasestorage.app",messagingSenderId:"238024057501",appId:"1:238024057501:web:0bf30048340f8e880daa74"};
-const app=initializeApp(firebaseConfig);
-export const db=getFirestore(app);
-export {doc,updateDoc,increment,onSnapshot};
 import {
-  db,
+  getFirestore,
   doc,
-  onSnapshot,
-  updateDoc
-} from './firebase.js';
+  updateDoc,
+  increment,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-document
-.getElementById("resetBtn")
-.addEventListener("click", async () => {
+const firebaseConfig = {
+  apiKey: "AIzaSyDEN1XSKmtIi-DlLihv65GOdNa6v0oMcfw",
+  authDomain: "construindo-o-templo.firebaseapp.com",
+  projectId: "construindo-o-templo",
+  storageBucket: "construindo-o-templo.firebasestorage.app",
+  messagingSenderId: "238024057501",
+  appId: "1:238024057501:web:0bf30048340f8e880daa74"
+};
 
-  const confirmar = confirm(
-    "Deseja realmente reiniciar a atividade?"
-  );
+const app = initializeApp(firebaseConfig);
 
-  if(!confirmar) return;
+export const db = getFirestore(app);
 
-  await updateDoc(
-    doc(db,"atividade","progresso"),
-    {
-      respostas: 0,
-      pontos: 0,
-      participantes: 0,
-      quadradosRevelados: 0
-    }
-  );
-
-});
+export {
+  doc,
+  updateDoc,
+  increment,
+  onSnapshot
+};
